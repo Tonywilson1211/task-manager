d8bac98394e4abc8c4f97b87d600dc938ce83761
Tonywilson1211 <>
2023-05-04 10:25:16 +0000

    file structure

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // sidenav initialisation
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
+
+// Or with jQuery
+
+// $(document).ready(function () {
+//     $('.sidenav').sidenav();
+// });
